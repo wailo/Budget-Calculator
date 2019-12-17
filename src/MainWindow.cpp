ecdf11f934d0e36403aeaff13efebdf58749269d
Osama Yahya <oisy92@gmail.com>
2019-12-18 00:35:40 +1000

    Project Restructure + Features Addition
    - Organaising the the project structure by seperating files intro categorized folder.
    - Enhancing the user interface panel to make it easier to input data.
    - Adding charts (pie and column chart) to visualize spending.

---
+++ src/MainWindow.cpp
@@ -0,0 +1,11 @@
+#include "../include/pch.h"
+
+
+using namespace std;
+using namespace System;
+using namespace System::ComponentModel;
+using namespace System::Collections;
+using namespace System::Windows::Forms;
+using namespace System::Data;
+using namespace System::Drawing;
+
